97e1e6befaa91b8b16b8481d2647178c5b59b724
unknown <rdn787@bk.ru>
2021-03-03 13:33:42 +0300

    added Ticket.java

--- src/main/java/ru/netology/domain/Ticket.java
+++ src/main/java/ru/netology/domain/Ticket.java
@@ -6,15 +6,10 @@ import lombok.Data;
 @AllArgsConstructor
 @Data
 
-public class Ticket implements Comparable<Ticket>{
+public class Ticket{
     private int id;
     private int price;
     private String departure;
     private String arrival;
     private int travelTime;
-
-    @Override
-    public int compareTo(Ticket o) {
-        return this.price - o.price;
-    }
 }
